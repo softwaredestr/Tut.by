3ff8102aaf80559d8454cf9733c97ef162927192
softwaredestr <eugeny.noskov@gmail.com>
2020-10-19 15:21:44 +0300

    Added new pages and tests

--- src/test/java/pages/OnlineTheatresPage.java
+++ src/test/java/pages/OnlineTheatresPage.java
@@ -1,12 +1,9 @@
 package pages;
 
 import io.qameta.allure.Step;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 import java.util.List;
